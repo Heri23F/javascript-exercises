d214a48a18e76a9857418ee4a9de55597a05ce5d
Heri23F <heri.github.com@gmail.com>
2026-03-10 14:31:27 +0700

    Complete JS foundation 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let addString = ""
 
-};
+    if (num < 0 ) {
+        return "ERROR"
+    } else {
+        for (let i = 0; i < num; i++) {
+        addString += string
+        }
+        return addString
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
