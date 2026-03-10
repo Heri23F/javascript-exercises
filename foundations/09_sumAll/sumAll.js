c4050240ea78d111aefb52e776293c0c11c8221b
Heri23F <heri.github.com@gmail.com>
2026-03-11 01:11:43 +0700

    Remove unsued comment

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -14,23 +14,10 @@ const sumAll = function(num1, num2) {
             sum += i
         }
     }
-    
+
     return sum
 };
 
 // Do not edit below this line
 module.exports = sumAll;
 
-// if (Number.isInteger(num1 && num2) !== true) {
-//         sum = "ERROR"
-//     }else if (num1 < 0 || num2 < 0) {
-//         sum = "ERROR"
-//     }else if (num1 < num2) {
-//         for (let i = num1; i <= num2; i++) {
-//         sum += i
-//         }
-//     } else if (num2 < num1) {
-//         for (let i = num2; i <= num1; i++) {
-//             sum += i
-//         }
-//     }
