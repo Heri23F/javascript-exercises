f33c5267ab376abf6d52f96f37e1ae4a8fbe71db
Heri23F <heri.github.com@gmail.com>
2026-03-11 01:05:27 +0700

    Complete JS exercise foundations 09_sumAll & test

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        sum = "ERROR"
+    } else if (num1 < 0 || num2 < 0) {
+        sum = "ERROR";
+        
+    } else if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+        sum += i
+        }
+    } else if (num2 < num1) {
+        for (let i = num2; i <= num1; i++) {
+            sum += i
+        }
+    }
+    
+    return sum
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// if (Number.isInteger(num1 && num2) !== true) {
+//         sum = "ERROR"
+//     }else if (num1 < 0 || num2 < 0) {
+//         sum = "ERROR"
+//     }else if (num1 < num2) {
+//         for (let i = num1; i <= num2; i++) {
+//         sum += i
+//         }
+//     } else if (num2 < num1) {
+//         for (let i = num2; i <= num1; i++) {
+//             sum += i
+//         }
+//     }
