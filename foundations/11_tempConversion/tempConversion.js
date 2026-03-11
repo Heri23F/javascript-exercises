bfa4bcb92cc40ed3a0078df5140a00eae2701657
Heri23F <heri.github.com@gmail.com>
2026-03-11 13:37:42 +0700

    Complete and testing JS exercise foundations 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let c = 5/9 * (fahrenheit-32)
+  return Math.round(c * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let f = (celcius * 1.8) + 32
+  return Math.round(f * 10) / 10
 };
 
 // Do not edit below this line
