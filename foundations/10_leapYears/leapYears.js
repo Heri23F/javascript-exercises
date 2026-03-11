3335d4843887d13aacb06f1fff2738b7646f2d12
Heri23F <heri.github.com@gmail.com>
2026-03-11 13:12:44 +0700

    Complete and test JS exercise foundations 10_leapYears for the first time

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if (years % 4 == 0  && (!(years % 100 == 0) || years % 400 == 0)) {
+        return "Lapyear"
+    } else {
+        return "Not Lapyear"
+    }
 };
 
 // Do not edit below this line
