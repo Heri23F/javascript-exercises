d47da568b687064500ee910068a9fcb9b1b6b9a9
Heri23F <heri.github.com@gmail.com>
2026-03-11 13:17:03 +0700

    Passed JS exercise foundations 10_leapYears test

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,8 +1,8 @@
 const leapYears = function(years) {
     if (years % 4 == 0  && (!(years % 100 == 0) || years % 400 == 0)) {
-        return "Lapyear"
+        return true
     } else {
-        return "Not Lapyear"
+        return false
     }
 };
 
